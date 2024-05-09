727485719f294bb031ded5ba29e74d29fd9dbd69
madaooftheblues <azlanrasool@gmail.com>
2024-05-09 19:59:22 +0500

    Implement method set(key, value)

--- hashmap.js
+++ hashmap.js
@@ -1,5 +1,6 @@
 function HashMap() {
     let _size = 16
+    let buckets = new Array(_size)
 
     function hash(key) {
         let hashCode = 0
@@ -13,5 +14,40 @@ function HashMap() {
         return hashCode
     }
 
-    return { hash }
+    function set(key, value) {
+        const index = hash(key)
+        if (index < 0 || index >= buckets.length) {
+            throw new Error('Trying to access index out of bound')
+        }
+
+        if (!buckets[index]) {
+            buckets[index] = { key, value }
+            return
+        }
+
+        if (buckets[index].key === key) {
+            buckets[index].value = value
+            return
+        }
+
+        if (Array.isArray(buckets[index])) {
+            const list = buckets[index]
+            for (let i = 0; i < list.length; i++) {
+                if (list[i].key === key) {
+                    list[i].value = value
+                    return
+                }
+            }
+
+            list.push({ key, value })
+            return
+        }
+
+        const list = []
+        list.push(buckets[index])
+        list.push({ key, value })
+        buckets[index] = list
+    }
+
+    return { set }
 }
